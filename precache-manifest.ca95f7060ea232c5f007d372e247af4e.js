3aa850001ef439051d13c7f72f0f98a82e5e7f19
vivz753 <vivz753@gmail.com>
2019-09-10 21:16:23 -0700

    attempt to serve react app

---
+++ precache-manifest.ca95f7060ea232c5f007d372e247af4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcc95c800b59a5809ad336ec841062f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9e3b717b118976c63d6",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "e1996cfad4fe613c1940",
+    "url": "/static/js/2.039b1ebe.chunk.js"
+  },
+  {
+    "revision": "d9e3b717b118976c63d6",
+    "url": "/static/js/main.b042426f.chunk.js"
+  },
+  {
+    "revision": "b579e401fc35518936a7",
+    "url": "/static/js/runtime~main.a970ad57.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ vleung-app/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          herp derp. lmao
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/vivz753"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          github.com/vivz753
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;

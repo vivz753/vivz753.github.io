452db327d7d6fe39c20748152298491ef02bf843
Vivian <vivz753@gmail.com>
2019-10-13 18:33:33 -0700

    install antd component library

--- vleung-app/src/App.js
+++ vleung-app/src/App.js
@@ -1,29 +1,34 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { MyLayout } from './layout/MyLayout';
+import { SelectedMenuItemProvider } from './context';
 import './App.css';
-import Cube from './Cube';
+import "antd/dist/antd.css";
+import "./index.css";
 
 class App extends Component {
 
   render() {
     return (
-      <div className="App">
-      <Cube></Cube>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            herp derp. lmao
-          </p>
-          <a
-            className="App-link"
-            href="https://github.com/vivz753"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            github.com/vivz753
-          </a>
-        </header>
-      </div>
+      <SelectedMenuItemProvider>   
+        <div className="App">
+          <MyLayout />
+          {/* <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              herp derp. lmao
+            </p>
+            <a
+              className="App-link"
+              href="https://github.com/vivz753"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              github.com/vivz753
+            </a>
+          </header> */}
+        </div>
+      </SelectedMenuItemProvider>
     );
   }
   

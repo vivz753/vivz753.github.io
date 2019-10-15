c917c93a31833be0bc2fdf0dd9914737c59c3e3b
Vivian <vivz753@gmail.com>
2019-10-14 20:25:09 -0700

    add function to find menu items in submenus for sidebar

--- vleung-app/src/helpers/index.js
+++ vleung-app/src/helpers/index.js
@@ -1,6 +1,17 @@
-import { MenuItems } from '../constants';
+import { MenuItems, ProjectItems } from '../constants';
 
-export const getMenuTitle = (key) => {
+export const getMenuName = (key) => {
   let found = MenuItems.find(item => item.key === key)
   return found.name;
+}
+
+export const getProjectName = (groupKey, itemKey) => {
+
+  let siderItemName = '';
+
+  let siderGroupName = ProjectItems.find(item => item.key === groupKey);
+  itemKey ? siderItemName = siderGroupName.submenu.find(item => item.key === itemKey).name : siderItemName = ''; 
+  siderGroupName = siderGroupName.name;
+  
+  return [siderGroupName, siderItemName];
 }
